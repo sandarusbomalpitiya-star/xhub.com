2de7f53a47471f3ae5d04fb1590504efc060fa0a
sandarusbomalpitiya-star <sandarusbomalpitiya@gmail.com>
2026-03-15 19:17:09 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "Uvey5eC34kWTB8ZMkiB46ZNh8a12",
-  authDomain: "bpgxhub.firebaseapp.com",
-  databaseURL: "https://bpgxhub-default-rtdb.firebaseio.com",
-  projectId: "bpgxhub",
-  storageBucket: "bpgxhub.firebasestorage.app",
-  messagingSenderId: "781508153938",
-  appId: "1:781508153938:web:aa716282bab0090e7cc58b"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyADbsAxb1ogmbcI2RjX7UJQ-7CTs7aaho0",
+    authDomain: "bgx-hub.firebaseapp.com",
+    databaseURL: "https://bgx-hub-default-rtdb.firebaseio.com",
+    projectId: "bgx-hub",
+    storageBucket: "bgx-hub.firebasestorage.app",
+    messagingSenderId: "872433084169",
+    appId: "1:872433084169:web:3be5386fc5a77d30d115d8",
+    measurementId: "G-8VN47CC4L6"
+  };
 
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
