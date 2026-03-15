2f9f13d56b13fb0ac6162686fb48e26fdb8e2c10
sandarusbomalpitiya-star <sandarusbomalpitiya@gmail.com>
2026-03-15 18:46:01 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDcKP6qgb8EbLh-dDbR43g3KZmNFCv5xzg",
-  authDomain: "bpgxhub.firebaseapp.com",
-  databaseURL: "https://bpgxhub-default-rtdb.firebaseio.com",
-  projectId: "bpgxhub",
-  storageBucket: "bpgxhub.firebasestorage.app",
-  messagingSenderId: "781508153938",
-  appId: "1:781508153938:web:aa716282bab0090e7cc58b"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDcKP6qgb8EbLh-dDbR43g3KZmNFCv5xzg",
+    authDomain: "bpgxhub.firebaseapp.com",
+    databaseURL: "https://bpgxhub-default-rtdb.firebaseio.com",
+    projectId: "bpgxhub",
+    storageBucket: "bpgxhub.firebasestorage.app",
+    messagingSenderId: "781508153938",
+    appId: "1:781508153938:web:aa716282bab0090e7cc58b"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
